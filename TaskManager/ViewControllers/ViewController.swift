a656f3d32780940786e733b786c16ec2267ce5c8
Пахаев Хусейн <pakhaev01@gmail.com>
2023-04-29 20:30:49 +0300

    Revert "Remove main.storyboard and view controller files"
    This reverts commit 74a27cabae45d23ea942c60f90df0ed61223862e.

---
+++ TaskManager/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskManager
+//
+//  Created by Khusain on 26.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
